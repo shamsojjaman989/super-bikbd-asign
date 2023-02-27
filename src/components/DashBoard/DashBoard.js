9fa225c44a8a925075c39fcbe66404f92fed3b07
Tushar-Kanti-Dev <81237222+shamsojjaman989@users.noreply.github.com>
2023-02-27 11:30:54 +0600

    dashboard update-(Rechart)

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,9 +1,48 @@
+
 import React from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import useBike from '../../hooks/CustomHooks/useBike';
+import './DashBoard.css'
+
+
+
 
 const DashBoard = () => {
+    const [data, setData] = useBike();
     return (
-        <div>
-            <h3>Dash Board</h3>
+        <div className='chart-container'>
+            <div className="area-chart">
+                <AreaChart width={530} height={250} data={data}
+                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                    </linearGradient>
+                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                    <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                    </linearGradient>
+                </defs>
+                <XAxis dataKey="name" />
+                <YAxis />
+                <CartesianGrid strokeDasharray="3 3" />
+                <Tooltip></Tooltip>
+                <Area type="monotone" dataKey="price" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                <Area type="monotone" dataKey="name" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                </AreaChart>
+            </div>
+            <div className="bar-chart">
+            <BarChart width={530} height={250} data={data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="price" fill="#8884d8" />
+                <Bar dataKey="name" fill="#82ca9d" />
+            </BarChart>
+            </div>
         </div>
     );
 };
